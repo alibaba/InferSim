b1b4eb84a98532bd13ebb5e5a227c7956d48dadf
MtFitzRoy <ys472446@alibaba-inc.com>
2025-11-20 16:59:50 +0800

    fix gdn conv states

--- kvcache/kvcache.py
+++ kvcache/kvcache.py
@@ -28,14 +28,18 @@ def get_kvcache_size(config: ModelConfig, use_fp8):
 
 def get_states_size(config: ModelConfig):
     conv_state_size = (
-        config.linear_num_value_heads * config.linear_value_head_dim
-        + 2 * config.linear_num_key_heads * config.linear_key_head_dim
-    ) * 2
+        (
+            config.linear_num_value_heads * config.linear_value_head_dim
+            + 2 * config.linear_num_key_heads * config.linear_key_head_dim
+        )
+        * (config.linear_conv_kernel_dim - 1)
+        * 2
+    )  # bf16
     ssm_state_size = (
         config.linear_num_value_heads
         * config.linear_key_head_dim
         * config.linear_value_head_dim
         * 4
-    )
+    )  # fp32
     states_size = config.num_linear_attn_layers * (conv_state_size + ssm_state_size)
     return states_size
