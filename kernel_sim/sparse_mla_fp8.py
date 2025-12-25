8d1903be3ed531d5e78673aa9b776fdfe9c7d659
sheshuchen <2726404063@qq.com>
2025-12-25 15:20:32 +0800

    update

--- kernel_sim/sparse_mla_fp8.py
+++ kernel_sim/sparse_mla_fp8.py
@@ -80,7 +80,7 @@ def sparse_mla_fp8(
     time_load_and_dequant_per_token = time_load_kv_per_token + time_dequant_per_token
 
     time_mma_per_block = BLOCK_M * time_mma_per_token
-    time_load_and_dequant_per_block = (BLOCK_M // 2) * time_load_and_dequant_per_token
+    time_load_and_dequant_per_block = BLOCK_M // 2 * time_load_and_dequant_per_token
 
     time_per_block = max(
         seq_len * time_load_and_dequant_per_block, seq_len * time_mma_per_block
