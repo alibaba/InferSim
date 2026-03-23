4a72df52d1becd45164aaab25d2116a495c2fda8
sheshuchen <2726404063@qq.com>
2026-03-23 11:32:55 +0800

    fixed

--- example/qwen3.5-122B-A10B/decode.sh
+++ example/qwen3.5-122B-A10B/decode.sh
@@ -7,6 +7,6 @@ python3 main.py --config-path hf_configs/qwen3.5-122B-A10B_config.json \
   --device-type H20 \
   --world-size 2 \
   --tp-size 2 \
-  --decode-bs 128 \
+  --decode-bs 168 \
   --use-fp8-gemm \
   --decode-only
